ef61976354590c247ac467622ad43ec256ae1fcd
iamadahn <vendonism16@yandex.ru>
2025-03-10 11:59:12 +0300

    tasks: motor: parsing input, moving motors

--- src/tasks/motor.rs
+++ src/tasks/motor.rs
@@ -7,14 +7,42 @@ use crate::InputData;
 
 #[embassy_executor::task]
 pub async fn motor_controller_task(
-    left_ch: SimplePwmChannel<'static, TIM1>,
-    right_ch: SimplePwmChannel<'static, TIM1>,
-    left_forward: Output<'static>,
-    left_backward: Output<'static>,
-    right_forward: Output<'static>,
-    right_backward: Output<'static>,
+    mut motor_l: SimplePwmChannel<'static, TIM1>,
+    mut motor_r: SimplePwmChannel<'static, TIM1>,
+    mut left_forward: Output<'static>,
+    mut left_backward: Output<'static>,
+    mut right_forward: Output<'static>,
+    mut right_backward: Output<'static>,
     input_sub: Receiver<'static, NoopRawMutex, InputData, 1>
     ) {
-    
+        motor_l.enable();
+        motor_r.enable();
+        loop {
+            let mut input = input_sub.receive().await;
+            if input.y1 > 50 {
+                left_forward.set_high();
+                left_backward.set_low();
+                right_forward.set_high();
+                right_backward.set_low();
+                input.y1 = (input.y1 - 50) * 2;
+            } else if input.y1 < 50 {
+                left_forward.set_low();
+                left_backward.set_high();
+                right_forward.set_low();
+                right_backward.set_high();
+                input.y1 = (50 - input.y1) * 2;
+            } else {
+                left_forward.set_low();
+                left_backward.set_low();
+                right_forward.set_low();
+                right_backward.set_low();
+            }
+
+            motor_l.set_duty_cycle_percent(input.y1);
+            motor_r.set_duty_cycle_percent(input.y1);
+            _ = input.x1;
+            _ = input.x2;
+            _ = input.y2;
+        }
 }
 
